6cd0d3305f0b1cba6ec609701d9aa4a78f9c34f8
junnKk <wnsrudrose@dongguk.edu>
2021-04-17 15:20:12 +0900

    complete ionic angular chat

--- ionic-chat/client/src/app/home/home.page.ts
+++ ionic-chat/client/src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import axios from 'axios';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username : string; 
 
   constructor() {}
-
+  login(){
+    axios.post('http://localhost:3030/login', {
+        username : this.username
+    })
+    .then(res=>{
+        if(res.data == 'fail!'){
+            alert("인증 실패!");
+            return;
+        }
+        sessionStorage.setItem("id", this.username);
+        location.href ="/chat";
+    })
+    .catch(error=>{
+        console.log(error);
+    })
+  }
 }
